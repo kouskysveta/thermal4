b322fe8809aecff2a748cf83d6d21a86c49ecc17
kouskysveta <38882997+kouskysveta@users.noreply.github.com>
2019-06-07 10:52:39 +0200

    change fps

--- camera/__init__.py
+++ camera/__init__.py
@@ -48,7 +48,7 @@ class ThermoAnalyzer:
         self.corr_param_a = 6,837
         self.corr_param_b = 2.6652*1.7134
 	self.corr_param_c = 42.605
-        self.fps = 5
+        self.fps = 2
 
     def __del__(self):
         self.run_camera_flag.clear()
